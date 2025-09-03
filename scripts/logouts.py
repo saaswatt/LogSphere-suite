45407ebc0ec87a4a15752d52a4b96ed565ddbbab
Saswat <saswatkumar.pandey@gmail.com>
2025-09-03 18:25:36 +0530

    Refactor LogSphere Logging System:
    -Renamed Scripts
    
    -Switched logs format from .txt to .jsonl
    
    -Updated .gitignore

---
+++ scripts/logouts.py
@@ -0,0 +1,18 @@
+import json
+from datetime import datetime
+import os
+
+log_entry = {
+    "time stamp " : datetime.now().astimezone().isoformat(),
+    "Event " : "logout"
+}
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+logs_dir = os.path.join(base_dir, "..", "logs")
+os.makedirs(logs_dir, exist_ok=True)
+
+log_file = os.path.join(logs_dir, "logout_logs.jsonl")
+
+with open(log_file, "a") as f:
+    f.write(json.dumps(log_entry)+"\n")
